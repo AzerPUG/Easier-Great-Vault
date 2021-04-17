84b9d745b2a08ec466947d8544873f783261bfb1
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2021-04-17 13:14:36 +0200

    Make Easier GreatVault work standalone and with Core.

--- EasierGreatVault.lua
+++ EasierGreatVault.lua
@@ -10,19 +10,40 @@ AZP.EasierGreatVault = {}
 local dash = " - "
 local name = "Easier GreatVault"
 local nameFull = ("AzerPUG " .. name)
-local promo = (nameFull .. dash ..  AZPIUGreatVaultVersion)
+local promo = (nameFull .. dash ..  AZP.VersionControl.EasierGreatVault)
+local optionHeader = "|cFF00FFFFEasier GreatVault|r"
+local AZPEGVSelfOptionPanel = nil
 
-local ModuleStats = AZP.IU.ModuleStats
 
-function AZP.VersionControl:EasierGreatVault()
-    return AZPIUGreatVaultVersion
+function AZP.EasierGreatVault:OnLoadSelf()
+    local eventFrame = CreateFrame("Frame")
+    eventFrame:SetScript("OnEvent", AZP.OnEvent.EasierGreatVault)
+    eventFrame:RegisterEvent("ADDON_LOADED")
+
+    AZPEGVSelfOptionPanel = CreateFrame("FRAME", nil)
+    AZPEGVSelfOptionPanel.name = optionHeader
+    InterfaceOptions_AddCategory(AZPEGVSelfOptionPanel)
+    AZPEGVSelfOptionPanel.header = AZPEGVSelfOptionPanel:CreateFontString(nil, "ARTWORK", "GameFontNormalHuge")
+    AZPEGVSelfOptionPanel.header:SetPoint("TOP", 0, -10)
+    AZPEGVSelfOptionPanel.header:SetText(optionHeader)
+
+    AZPEGVSelfOptionPanel.footer = AZPEGVSelfOptionPanel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+    AZPEGVSelfOptionPanel.footer:SetPoint("TOP", 0, -300)
+    AZPEGVSelfOptionPanel.footer:SetText(
+        "|cFF00FFFFAzerPUG Links:\n" ..
+        "Website: www.azerpug.com\n" ..
+        "Discord: www.azerpug.com/discord\n" ..
+        "Twitch: www.twitch.tv/azerpug\n|r"
+    )
+    AZP.EasierGreatVault.FillOptionsPanel(AZPEGVSelfOptionPanel)
 end
 
-function AZP.OnLoad:EasierGreatVault(self)
-    ModuleStats["Frames"]["EasierGreatVault"]:SetSize(150, 50)
-    addonMain:ChangeOptionsText()
+function AZP.EasierGreatVault:OnLoadCore()
+    AZP.Core.ModuleStats["Frames"]["EasierGreatVault"]:SetSize(150, 50)
+    AZP.Core:RegisterEvents("ADDON_LOADED", AZP.EasierGreatVault.eventOnOtherAddonLoaded)
+    AZP.OptionsPanels:Generic("Easier Great Vault", optionHeader, AZP.EasierGreatVault.FillOptionsPanel)
 
-    local AZPGVButton = CreateFrame("Button", nil, ModuleStats["Frames"]["EasierGreatVault"], "UIPanelButtonTemplate")
+    local AZPGVButton = CreateFrame("Button", nil, AZP.Core.ModuleStats["Frames"]["EasierGreatVault"], "UIPanelButtonTemplate")
     AZPGVButton.contentText = AZPGVButton:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
     AZPGVButton.contentText:SetText("Open Great Vault!")
     AZPGVButton:SetWidth("100")
@@ -39,53 +60,62 @@ function AZP.OnLoad:EasierGreatVault(self)
             WeeklyRewardsFrame:Show()
         end
     end )
-
-    local AZPGVCovButton
-
-    AZP.AddonHelper:DelayedExecution(10, 
-    function()  
-        AZPGVCovButton = CreateFrame("Button", nil, GarrisonLandingPage, "UIPanelButtonTemplate")
-        AZPGVCovButton.contentText = AZPGVCovButton:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
-        AZPGVCovButton.contentText:SetText("Great Vault!")
-        AZPGVCovButton:SetWidth("75")
-        AZPGVCovButton:SetHeight("25")
-        AZPGVCovButton.contentText:SetWidth("75")
-        AZPGVCovButton.contentText:SetHeight("25")
-        AZPGVCovButton:SetPoint("TOPLEFT", 100, -10)
-        AZPGVCovButton.contentText:SetPoint("CENTER", 0, -1)
-        AZPGVCovButton:SetScript("OnClick",
-        function()
-            LoadAddOn("Blizzard_WeeklyRewards")
-            if WeeklyRewardsFrame:IsShown() then
-                WeeklyRewardsFrame:Hide()
-            else
-                WeeklyRewardsFrame:Show()
-            end
-        end )
-    end)
 end
 
-function AZP.OnEvent:EasierGreatVault(event, ...)
+function AZP.EasierGreatVault.eventOnOtherAddonLoaded(addon)
+    if addon == "Blizzard_GarrisonUI" or IsAddOnLoaded("Blizzard_GarrisonUI") then
+        if GarrisonLandingPage ~= nil and GarrisonLandingPage.AZPGVCovButton == nil then
+            GarrisonLandingPage.AZPGVCovButton = CreateFrame("Button", nil, GarrisonLandingPage, "UIPanelButtonTemplate")
+            GarrisonLandingPage.AZPGVCovButton.contentText = GarrisonLandingPage.AZPGVCovButton:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
+            GarrisonLandingPage.AZPGVCovButton.contentText:SetText("Great Vault!")
+            GarrisonLandingPage.AZPGVCovButton:SetWidth("75")
+            GarrisonLandingPage.AZPGVCovButton:SetHeight("25")
+            GarrisonLandingPage.AZPGVCovButton.contentText:SetWidth("75")
+            GarrisonLandingPage.AZPGVCovButton.contentText:SetHeight("25")
+            GarrisonLandingPage.AZPGVCovButton:SetPoint("TOPLEFT", 100, -10)
+            GarrisonLandingPage.AZPGVCovButton.contentText:SetPoint("CENTER", 0, -1)
+            GarrisonLandingPage.AZPGVCovButton:SetScript("OnClick",
+            function()
+                LoadAddOn("Blizzard_WeeklyRewards")
+                if WeeklyRewardsFrame:IsShown() then
+                    WeeklyRewardsFrame:Hide()
+                else
+                    WeeklyRewardsFrame:Show()
+                end
+            end )
+        end
+    end
 end
 
-function AZP.EasierGreatVault:ChangeOptionsText()
-    GreatVaultSubPanelPHTitle:Hide()
-    GreatVaultSubPanelPHText:Hide()
-    GreatVaultSubPanelPHTitle:SetParent(nil)
-    GreatVaultSubPanelPHText:SetParent(nil)
 
-    local GreatVaultSubPanelHeader = GreatVaultSubPanel:CreateFontString("GreatVaultSubPanelHeader", "ARTWORK", "GameFontNormalHuge")
-    GreatVaultSubPanelHeader:SetText(promo)
-    GreatVaultSubPanelHeader:SetWidth(GreatVaultSubPanel:GetWidth())
-    GreatVaultSubPanelHeader:SetHeight(GreatVaultSubPanel:GetHeight())
-    GreatVaultSubPanelHeader:SetPoint("TOP", 0, -10)
+function AZP.OnEvent.EasierGreatVault(self, event, ...)
+    if event == "ADDON_LOADED" then
+        AZP.EasierGreatVault.eventOnOtherAddonLoaded(...)
+    end
+end
 
-    local GreatVaultSubPanelText = GreatVaultSubPanel:CreateFontString("GreatVaultSubPanelText", "ARTWORK", "GameFontNormalLarge")
-    GreatVaultSubPanelText:SetWidth(GreatVaultSubPanel:GetWidth())
-    GreatVaultSubPanelText:SetHeight(GreatVaultSubPanel:GetHeight())
-    GreatVaultSubPanelText:SetPoint("TOPLEFT", 0, -50)
-    GreatVaultSubPanelText:SetText(
-        "AzerPUG's Easier GreatVault' does not have options yet.\n" ..
-        "For feature requests visit our Discord Server!"
-    )
+function AZP.EasierGreatVault.FillOptionsPanel(frameToFill)
+    -- GreatVaultSubPanelPHTitle:Hide()
+    -- GreatVaultSubPanelPHText:Hide()
+    -- GreatVaultSubPanelPHTitle:SetParent(nil)
+    -- GreatVaultSubPanelPHText:SetParent(nil)
+
+    -- local GreatVaultSubPanelHeader = GreatVaultSubPanel:CreateFontString("GreatVaultSubPanelHeader", "ARTWORK", "GameFontNormalHuge")
+    -- GreatVaultSubPanelHeader:SetText(promo)
+    -- GreatVaultSubPanelHeader:SetWidth(GreatVaultSubPanel:GetWidth())
+    -- GreatVaultSubPanelHeader:SetHeight(GreatVaultSubPanel:GetHeight())
+    -- GreatVaultSubPanelHeader:SetPoint("TOP", 0, -10)
+
+    -- local GreatVaultSubPanelText = GreatVaultSubPanel:CreateFontString("GreatVaultSubPanelText", "ARTWORK", "GameFontNormalLarge")
+    -- GreatVaultSubPanelText:SetWidth(GreatVaultSubPanel:GetWidth())
+    -- GreatVaultSubPanelText:SetHeight(GreatVaultSubPanel:GetHeight())
+    -- GreatVaultSubPanelText:SetPoint("TOPLEFT", 0, -50)
+    -- GreatVaultSubPanelText:SetText(
+    --     "AzerPUG's Easier GreatVault' does not have options yet.\n" ..
+    --     "For feature requests visit our Discord Server!"
+    -- )
+end
+
+if not IsAddOnLoaded("AzerPUG's Core") then
+    AZP.EasierGreatVault:OnLoadSelf()
 end
